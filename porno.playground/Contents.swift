fc98d19e345270c1c6c0bd5f5925e20d9179d195
Alumno <alumno@iMac-de-IT-Admin.local>
2018-02-06 19:59:42 -0600

    porno

---
+++ porno.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var now = Data()
+var pastData = Date(timeIntervalSinceNow: -120)
+
+print("Kill la kill me")
+
+var dos = 2
+var tres = 3
+if (dos==2){
+print ("ass")
+}
